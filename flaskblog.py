e08f8cf6041ba258da73be8da19743c8427ea4ed
Neelam <neelam.singhal1304@gmail.com>
2022-09-20 11:22:50 +0530

    Add: Python web development

---
+++ flaskblog.py
@@ -0,0 +1,48 @@
+## Steps:
+## To run the file, enter the following commands
+# set FLASK_APP=flaskblog.py
+# $env:FLASK_APP="flaskblog.py"
+# flask run
+
+## If you don't want to restart the server everytime, do the floowing:
+# set FLASK_DEBUG=1
+# $env:FLASK_DEBUG='1'
+
+## Templating engine that flask uses is called Jinja 2
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+
+    {
+        'author':'Neelam Singhal',
+        'title': 'Blog post 1',
+        'content': 'This is first blog',
+        'date_posted': '20-09-2022'
+    },
+    {
+        'author':'Monali Mehta',
+        'title': 'Blog post 2',
+        'content': 'This is Second blog',
+        'date_posted': '21-09-2022'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('home.html', post=posts)
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
+
+
